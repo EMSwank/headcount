6ecc45d5baff880d2c7ea3b3da7313488eefe925
Eliot Swank <eliotswank@gmail.com>
2018-03-13 15:49:51 -0600

    adds functunal kindergarten_participation_by_year method

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -11,9 +11,10 @@ class Enrollment
   def kindergarten_participation_by_year
     new_hash = {}
     @kindergarten_participation.each_pair do |year, value|
-      converted_value = value.to_s.split(".")
-      new_value = converted_value[1][0..1].join(".")
-      new_hash[year] = new_value
+      converted_value = value.to_s.split(".")[1][0..2]
+      new_value = '0.' + converted_value
+      new_hash[year] = new_value.to_f
+      #  binding.pry
     end
     new_hash
   end
