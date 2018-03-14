9141a6749442da7be99eaf5cb7d8a6f663483e71
Eliot Swank <eliotswank@gmail.com>
2018-03-14 13:01:30 -0600

    finish work on district repo find_by_name method

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,25 +1,22 @@
 require 'CSV'
-require './lib/district'
+require_relative 'district'
 
 class DistrictRepository
 
   attr_reader :districts
 
-  def initialize
-    @districts = []
-  end
-
-  def load_data(file)
-    contents = CSV.open(file, headers: true, header_converters: :symbol)
-      # binding.pry
-    @districts = contents.map do |row|
-      District.new(row)
-    end
-    districts.uniq! {|district| district.name}
+  def load_data(symbols)
+      enrollment = symbols[:enrollment]
+      data = enrollment[:kindergarten]
+      contents = CSV.open(data, {headers: true, header_converters: :symbol})
+      @districts = contents.map do |row|
+        row[:name] = row[:location]
+        District.new(row)
+      end
+      return contents
   end
 
   def find_by_name(name)
-    binding.pry
     @districts.find do |district|
       district.name == name
     end
--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -11,21 +11,20 @@ class DistrictRepositoryTest < Minitest::Test
 
   def test_load_data
     dr = DistrictRepository.new
-    file = './test/fixtures/kinder_full_day.csv'
+    expected = dr.load_data({ :enrollment => {
+              :kindergarten => './test/fixtures/kinder_full_day.csv'}
+            })
 
-    expected = dr.load_data(file)
-
-    assert_instance_of Array, expected
+    assert_instance_of CSV, expected
   end
 
   def test_it_finds_by_name
     dr = DistrictRepository.new
-    # binding.pry
     dr.load_data({ :enrollment => {
             :kindergarten => "./data/Kindergartners in full-day program.csv"}
           })
-    # district = dr.find_by_name("ACADEMY 20")
+    district = dr.find_by_name("ACADEMY 20")
 
-    assert_equal "A 20", district
+    assert_instance_of District, district
   end
 end
