6649ce40302e7bf3258337f2ee2c66027fdb21f0
Eliot Swank <eliotswank@gmail.com>
2018-03-15 17:07:21 -0600

    fixes broken EnrollmentRepo tests

--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -12,7 +12,7 @@ class EnrollmentRepositoryTest < Minitest::Test
   def test_it_takes_a_file
     er = EnrollmentRepository.new
 
-    assert_instance_of CSV, er.load_data({
+    assert_instance_of Array, er.load_data({
     :enrollment => {
     :kindergarten => "./data/Kindergartners in full-day program.csv"
       }
@@ -25,6 +25,6 @@ class EnrollmentRepositoryTest < Minitest::Test
       :kindergarten => "./data/Kindergartners in full-day program.csv"
         }
       })
-    assert_instance_of EnrollmentRepository, er.find_by_name("Academy 20")
+    assert_instance_of Enrollment, er.find_by_name("Academy 20")
   end
 end
