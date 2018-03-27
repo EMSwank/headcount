5bfa69fa6e77c52534d4c7f76b8b5e0e11ed7d1d
Eliot Swank <eliotswank@gmail.com>
2018-03-27 15:01:00 -0600

    finishes graduation_rate_in_year

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -61,4 +61,17 @@ class EnrollmentTest < Minitest::Test
 
     assert_equal expected, actual
   end
+
+  def test_graduation_rate_in_year
+    er = EnrollmentRepository.new
+    er.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv",
+        :high_school_graduation => "./data/High school graduation rates.csv"
+      }
+    })
+    enrollment = er.find_by_name("ACADEMY 20")
+
+    assert_equal 0.895, enrollment.graduation_rate_in_year(2010)
+  end
 end
