a82d4f76ec3441ebb50dfb7343f03fe98a4e024a
Eliot Swank <eliotswank@gmail.com>
2018-04-05 14:46:23 -0600

    continue work on economic profile repo

--- lib/economic_profile_repository.rb
+++ lib/economic_profile_repository.rb
@@ -1,18 +1,31 @@
 require 'CSV'
+require_relative 'economic_profile'
 require_relative 'general_calculations'
 
 class EconomicProfileRepository
   include GeneralCalculations
 
+  attr_reader :profiles
   def load_data(data)
     @median_household_income = data[:economic_profile][:median_household_income]
     @children_in_poverty = data[:economic_profile][:children_in_poverty]
     @free_or_reduced_price_lunch = data[:economic_profile]
                                        [:free_or_reduced_price_lunch]
     @title_i = data[:economic_profile][:title_i]
+    @profiles = get_profiles(@title_i)
+    uniq_profiles
   end
 
-  def get_profiles
-    
+  def get_profiles(source)
+    profiles = get_data(source)
+    profiles.map do |row|
+      row[:name] == row[:location].upcase
+      EconomicProfile.new({:name => row[:name]})
+    end
+  end
+
+  def uniq_profiles
+    profiles.uniq! {|profile| profile.name}
+    # require 'pry'; binding.pry
   end
 end
