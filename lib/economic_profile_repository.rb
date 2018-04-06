7bd28f7a91d66df176bbfe8e792efc55199c6785
Eliot Swank <eliotswank@gmail.com>
2018-04-05 20:25:41 -0600

    rebuild after Atom crash and deleted EconomicProfileRepository

--- lib/economic_profile_repository.rb
+++ lib/economic_profile_repository.rb
@@ -1,4 +1,3 @@
-require 'CSV'
 require_relative 'economic_profile'
 require_relative 'general_calculations'
 
@@ -6,30 +5,34 @@ class EconomicProfileRepository
   include GeneralCalculations
 
   attr_reader :profiles
+
   def load_data(data)
     @median_household_income = data[:economic_profile][:median_household_income]
     @children_in_poverty = data[:economic_profile][:children_in_poverty]
-    @free_or_reduced_price_lunch = data[:economic_profile]
-                                       [:free_or_reduced_price_lunch]
+    @free_or_reduced_lunch = data[:economic_profile][:free_or_reduced_price_lunch]
     @title_i = data[:economic_profile][:title_i]
     @profiles = get_profiles(@title_i)
     uniq_profiles
-    add_median_household_incomes
+    get_medium_household_incomes
   end
 
   def get_profiles(source)
-    profiles = get_data(source)
-    profiles.map do |row|
+    data = get_data(source)
+    data.map do |row|
       row[:name] = row[:location].upcase
       EconomicProfile.new({:name => row[:name]})
     end
   end
 
-  def add_median_household_incomes
+  def uniq_profiles
+    profiles.uniq! {|profile| profile.name}
+  end
+
+  def get_medium_household_incomes
     data = get_data(@median_household_income)
     data.each do |row|
       parse_median_incomes(row)
-      index = find_index_for_profile(row)
+      index = find_index_for_profiles(row)
       profiles[index].median_household_income[row[:timeframe]] = row[:data]
     end
     profiles
@@ -39,28 +42,25 @@ class EconomicProfileRepository
     row[:name] = row[:location].upcase
     row[:timeframe] = row[:timeframe].split('-')
     row[:timeframe] = convert_to_integer(row[:timeframe])
-    if row[:data].to_i != Integer
-      row[:data] == 'N/A'
+    if row[:data].to_i.is_a? Integer
+      row[:data] = row[:data].to_i
     else
-      row[:data] == row[:data].to_i
+      row[:data] = 'N/A'
     end
   end
 
-  def uniq_profiles
-    profiles.uniq! {|profile| profile.name}
+  def convert_to_integer(numbers)
+    numbers.map do |number|
+      number.to_i
+    end
+  end
+
+  def find_index_for_profiles(row)
+    profiles.find_index {|profile| profile.name == row[:location].upcase}
   end
 
   def find_by_name(name)
     profiles.find {|profile| profile.name == name}
   end
 
-  def find_index_for_profile(row)
-    profiles.find_index do |profile|
-      profile.name == row[:location].upcase
-    end
-  end
-
-  def convert_to_integer(numbers)
-    numbers.map {|year| year.to_i}
-  end
 end
